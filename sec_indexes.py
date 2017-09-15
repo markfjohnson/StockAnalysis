1c59df7216532f4f751d88dd0d5545af537c4f18
markjohnson <markfjohnson@gmail.com>
2017-09-15 14:53:33 -0400

    changed topic reference to bytes

--- sec_indexes.py
+++ sec_indexes.py
@@ -44,7 +44,7 @@ def process_SEC_rss(year, month):
                      fileNumber=filingInfo['edgar:fileNumber'],
                      filingInfo=filingInfo['edgar:period'],
                      fiscalYearEnd=filingInfo['edgar:fiscalYearEnd'])
-        producer.send('sec_filing',newRow)
+        producer.send(b'sec_filing',newRow)
 
 
 if __name__ == "__main__":
