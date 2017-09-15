7abe6728842813b681d77cafb1393ccfe65f167a
markjohnson <markfjohnson@gmail.com>
2017-09-15 17:18:37 -0400

    fixed typo

--- scan_sec_filing_topic.py
+++ scan_sec_filing_topic.py
@@ -3,16 +3,36 @@ from pyspark.sql import *
 
 __all__ = ["SparkSession"]
 
-
-
-kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
 spark = SparkSession.builder \
-            .master("local") \
-            .appName("Read SEC XBRL RSS files into Kafka") \
-            .getOrCreate()
+    .master("local") \
+    .appName("Read SEC XBRL Kafka references to SEC filings") \
+    .getOrCreate()
 
-consumer = KafkaConsumer(bootstrap_servers=kafka_url,auto_offset_reset='earliest')
-consumer.subscribe(['sec_filing'])
+sc = spark.sparkContext()
 
-for message in consumer:
-    print (message)
+def analyze_and_save_filing(new_filing):
+    print (new_filing)
+    print ("ABC"+"EFG")
+
+def process_sec_filings():
+    kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
+
+    consumer = KafkaConsumer(bootstrap_servers=kafka_url,auto_offset_reset='earliest')
+    consumer.subscribe(['sec_filing'])
+
+    current_filings = []
+    for message in consumer:
+        current_filings.append(message)
+
+    found_messages = len(current_filings)
+    print("Found {} new filings.", found_messages)
+
+    if (found_messages > 0):
+        filings = sc.parallelize(current_filings)
+        processed_filings = filings.map( lambda f: analyze_and_save_filing(f))
+        print(processed_filings)
+
+
+
+if __name__ == "__main__":
+    process_sec_filings()
