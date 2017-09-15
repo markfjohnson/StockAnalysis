97aefb8e62b2f020861de4b20e9f4bb3f7e38fd2
markjohnson <markfjohnson@gmail.com>
2017-09-15 15:37:48 -0400

    fixed typo

--- scan_sec_filing_topic.py
+++ scan_sec_filing_topic.py
@@ -1,24 +1,18 @@
-import xmltodict
-import json
-
-# sample program
-
 from kafka import KafkaConsumer
 from pyspark.sql import *
 
 __all__ = ["SparkSession"]
 
 
-#kafka_url = "api.kafka.marathon.l4lb.thisdcos.directory:80"
+
 kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
 spark = SparkSession.builder \
             .master("local") \
             .appName("Read SEC XBRL RSS files into Kafka") \
             .getOrCreate()
 
-consumer = KafkaConsumer(bootstrap_servers=kafka_url,
-                                 auto_offset_reset='earliest')
-        consumer.subscribe(['sec_filing'])
+consumer = KafkaConsumer(bootstrap_servers=kafka_url,auto_offset_reset='earliest')
+consumer.subscribe(['sec_filing'])
 
-        for message in consumer:
-            print (message)
+for message in consumer:
+    print (message)
