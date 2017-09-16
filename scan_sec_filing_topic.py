a839db761d86b663ba5b44fbd9d6f1a52ffa29d5
markjohnson <markfjohnson@gmail.com>
2017-09-15 22:19:39 -0400

    fixed typo

--- scan_sec_filing_topic.py
+++ scan_sec_filing_topic.py
@@ -27,7 +27,7 @@ def process_sec_filings():
 
     while True :
         filings = sc.parallelize(consumer.poll(50))
-        print("Processing {} new filings",len(filings))
+        print("Processing {} new filings",filings.count())
         filings.map( lambda f: analyze_and_save_filing(f))
 
 
