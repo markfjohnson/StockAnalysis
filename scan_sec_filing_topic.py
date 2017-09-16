f2aeb3dadfd49e2d63691b137ab03289f9e9af80
markjohnson <markfjohnson@gmail.com>
2017-09-15 22:16:51 -0400

    fixed typo

--- scan_sec_filing_topic.py
+++ scan_sec_filing_topic.py
@@ -10,35 +10,25 @@ spark = SparkSession.builder \
 
 sc = spark.sparkContext
 
+
 def analyze_and_save_filing(new_filing):
     print("-------------")
     print (new_filing)
     print ("ABC"+"EFG")
+
 
 def process_sec_filings():
     kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
 
     consumer = KafkaConsumer(bootstrap_servers=kafka_url,auto_offset_reset='earliest')
     consumer.subscribe(['sec_filing'])
+    print("Consumer length")
     print("Completed subscription")
 
-    current_filings = []
-    for message in consumer:
-        print(message)
-        current_filings.append(message)
-        
-    print("A")
-    found_messages = len(current_filings)
-    print("B")
-    print("Found {} new filings.", found_messages)
-
-    if (found_messages > 0):
-        print("C")
-        filings = sc.parallelize(current_filings)
-        print("D")
-        processed_filings = filings.map( lambda f: analyze_and_save_filing(f))
-        print("E")
-        print(processed_filings)
+    while True :
+        filings = sc.parallelize(consumer.poll(50))
+        print("Processing {} new filings",len(filings))
+        filings.map( lambda f: analyze_and_save_filing(f))
 
 
 
