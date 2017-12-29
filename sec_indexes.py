d15cc7a86580a12dc412a40b23ea8df2fd7f9774
Mark Johnson <mfjohnson98@yahoo.com>
2017-12-28 23:55:38 -0500

    changed to cluster

--- sec_indexes.py
+++ sec_indexes.py
@@ -19,7 +19,8 @@ kafka_url = "broker.kafka.l4lb.thisdcos.directory:9092"
 topic_name = "sec_filings"
 
 spark = SparkSession.builder \
-    .master("192.168.1.100") \
+    .master("spark://192.168.1.100") \
+    .config("spark.submit.deployMode","cluster")
     .appName("Read SEC XBRL RSS files into Kafka") \
     .getOrCreate()
 sc = spark.sparkContext
